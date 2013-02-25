43e1f1bbcd40f383de173a3a5223efe16c0d7b02
Jen Wachter <jwachter@jhu.edu>
2013-02-25 11:51:17 -0500

    Initial commit.

---
+++ hubsterJS.js
@@ -0,0 +1,16 @@
+var hubster = (function (global, $) {
+
+	// closure scoped properties and methods
+	
+	// Return the object that will be globally accessible
+	return {
+		init: function() {
+
+		},
+		somethingElse: function()
+		{
+			
+		}
+	}
+
+})(window, jQuery);
